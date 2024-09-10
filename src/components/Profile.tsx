7bad6377e0bff7220966e0f03f14a12682a9d912
itsmefminsaf <itsmefminsaf@gmail.com>
2024-09-10 14:20:26 +0530

    some styling

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="child aspect-square">
+    <div className="child aspect-square max-sm:aspect-video max-md:aspect-video">
       <img
         src="/profilepic.jpg"
         alt="profile"
-        className="rounded-full border-4 p-2 shadow-2xl shadow-slate-200"
+        className=" rounded-full border-4 p-2 shadow-xl hover:shadow-2xl duration-500 hover:shadow-slate-200 shadow-slate-200 max-sm:w-1/2 max-md:w-1/2 m-auto"
       />
     </div>
   );
