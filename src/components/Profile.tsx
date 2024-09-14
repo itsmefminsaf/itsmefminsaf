9e6fa80f71e4496235b96fd77de864af98a3929a
itsmefminsaf <itsmefminsaf@gmail.com>
2024-09-14 12:18:18 +0530

    replaced img with Image for better image optimization

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import profile from "../assets/profile.jpg";
+import Image from "next/image";
 
 const Profile = () => {
   return (
     <div className="grid-item row-span-2 flex items-center justify-center aspect-square">
-      <img
+      <Image
         src={profile.src}
         alt="profile picture"
         className="w-[90%] rounded-full shadow-xl hover:shadow-white duration-500"
