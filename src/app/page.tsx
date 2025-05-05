f0dab9d078e748c189b15bae78d742403e49b6b2
Insaf <itsmefminsaf@gmail.com>
2025-05-05 17:58:11 +0530

    chore: changed pending text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center w-screen h-screen">
       <div className="border-[1px] border-gray-700/50 bg-black/20 backdrop-opacity-50 font-bold text-2xl p-5 rounded-2xl text-white">
-        Site is under construction
+        This portfolio will be available soon
       </div>
     </main>
   );
