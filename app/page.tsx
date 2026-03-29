96b4037ad84f3912fe8270a3e53656b1bc7985f5
itsmefminsaf <itsmefminsaf@gmail.com>
2026-03-29 23:48:41 +0530

    feat: add About component to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 };
