ab0af6184a104fe0cba67df42607ec5229c90a4c
itsmefminsaf <itsmefminsaf@gmail.com>
2026-03-29 16:00:59 +0530

    feat: implement profile header with social links and add react-icons dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 const HomePage = () => {
-  return (
-    <h1 className="p-5 text-center text-3xl font-extrabold">
-      Welcome to my portfolio
-    </h1>
-  );
+  return;
 };
 
 export default HomePage;
